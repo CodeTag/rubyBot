b5d4501c9fce5da11265e6bced6def50a0db913b
Julian Vargas <julianvargasalvarez@gmail.com>
2012-06-26 18:15:40 -0500

    fixes

--- bomberbot.rb
+++ bomberbot.rb
@@ -1,55 +1,45 @@
 require 'socket'
 
 class BomberBot
-    @in_from_server = nil
-    @socket_cliente = nill
-    @bot = Nothing
-
-    @conectado = false
-
-    def initialize()
+    def initialize
         begin
-            conectar("rbBot", "984198716")
-            controlConexion()
+            self.conectar("rbBot", "984198716")
+            self.controlConexion()
         rescue StandardError => e
             puts e
-            Console.ReadKey()
-        End Try
+        end
     end
 
-private
     def conectar user, token
-        socket_cliente = TCPSocket.open('localhost', 5000)
+        @socket_cliente = TCPSocket.open('localhost', 5000)
         bienvenida = socket_cliente.read
         puts bienvenida
-        socketCliente.puts(user + "," + token)
-        conectado = true
+        @socket_cliente.puts(user + "," + token)
+        @conectado = true
     end
 
     def controlConexion
         response = nil
-        while conectado do
+        while @conectado do
             puts "turno"
 
             server_message = socket_cliente.read
             
             message = serverMessage.split(";")
 
-            if message.length == 0
-                continue
-            end
-
             if message[0] == "EMPEZO"
                 bot = Bot.new(message[2][0])
                 bot.updateMap(message[1])
-            else if message[0] == "TURNO"
+            elsif message[0] == "TURNO"
                 puts "turno: " + message[1]
                 bot.updateMap message[2]
                 msg = bot.move
                 socket_cliente.puts(msg)
-            else if message[0] == "PERDIO"
+            elsif message[0] == "PERDIO"
                 puts "perdi :("
             end
         end
     end
 end
+
+bot = BomberBot.new
